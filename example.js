6ce6260b58f4c9ce7620ad36aaef1ed1fde1c0c5
Sergey Chikuyonok <serge.che@gmail.com>
2015-07-03 21:08:31 +0300

    More code refactoring

--- example.js
+++ example.js
@@ -1,12 +1,12 @@
 'use strict';
 
-var Template = require('./lib/template');
+var TemplateContext = require('./lib/context/template');
 var parseXml = require('./lib/parse-xml').parseFile;
 
 var templateXml = parseXml('./data/template.xml');
 var modelXml = parseXml('./data/model.xml');
 
-var template = new Template(templateXml);
+var template = new TemplateContext(templateXml);
 var result = template.transform(modelXml);
 
 console.log(result.innerHTML);
