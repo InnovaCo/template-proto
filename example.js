6fe8e6d5b862110af85a507fd87a772a13ad9a5c
Sergey Chikuyonok <serge.che@gmail.com>
2016-09-01 01:43:49 +0300

    Перенос реализации UVDOM

--- example.js
+++ example.js
@@ -1,7 +1,8 @@
 'use strict';
 
-var TemplateContext = require('./lib/context/template');
-var parseXml = require('./lib/parse-xml').parseFile;
+const TemplateContext = require('./lib/context/template');
+const UVDOMContext = require('./lib/context/uvdom');
+const parseXml = require('./lib/parse-xml').parseFile;
 
 var templateXml = parseXml('./data/template.xml');
 var modelXml = parseXml('./data/model.xml');
@@ -10,3 +11,7 @@ var template = new TemplateContext(templateXml);
 var result = template.transform(modelXml);
 
 console.log(result.innerHTML);
+
+var uvdom = new UVDOMContext();
+uvdom.transform(modelXml);
+console.log(uvdom.output.toJSON());
